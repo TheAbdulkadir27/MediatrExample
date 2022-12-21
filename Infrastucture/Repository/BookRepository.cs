3e7205cc0a68f8dd3df5d97bef1f4469541a80bc
TheAbdulkadir27 <abdulkadiryonucuoglu27@gmail.com>
2022-12-21 12:06:12 +0300

    Added Book Repository Include And ThenInclude

--- Infrastucture/Repository/BookRepository.cs
+++ Infrastucture/Repository/BookRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Domain.Common;
 using Domain.Entity;
 using Infrastucture.Context;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -31,7 +32,7 @@ namespace Infrastucture.Repository
         }
         public Books BookGetById(Guid id)
         {
-            return dataContext.Books.FirstOrDefault(v => v.Id == id);
+            return dataContext.Books.Include(v => v.BooksAuthors).ThenInclude(v => v.Writers).FirstOrDefault(v => v.Id == id);
         }
         public List<Books> PageMaxAndMin(Specification<Books> specification)
         {
